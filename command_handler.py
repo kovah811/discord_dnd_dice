bd6829b3f45630a324a807022c437f7738473639
kovah811 <36887070+kovah811@users.noreply.github.com>
2019-01-27 22:42:09 +0000

    refactoring command handler

--- command_handler.py
+++ command_handler.py
@@ -9,27 +9,40 @@ class CommandHandler:
 
     def command_handler(self, message):
         for command in self.commands:
-            if message.content.startswith(command['trigger']):
-                args = message.content.split(' ')
-                if args[0] == command['trigger']:
-                    args.pop(0)
-                    if command['req_args_num'] == 0:
-                        return (
-                            self.client.send_message(message.channel,
-                                                     str(command['function']
-                                                         (message, self.client,
-                                                          args)))
-                        )
-                    else:
-                        if len(args) >= command['req_args_num']:
-                            return (
-                                self.client.send_message(message.channel,
-                                                         str(command['function']
-                                                             (message, self.client,
-                                                              args)))
-                            )
-                        else:
-                            return (
-                                self.client.send_message(message.channel, 'command "{}" requires {} argument(s) "{}"'
-                                                         .format(command['trigger'], command['args_num'], ', '.join(command['args_name'])))
-                            )
+
+            if not message.content.startswith(command['trigger']):
+                continue
+
+            args = message.content.split(' ')
+            if args[0] != command['trigger']:
+                continue
+
+            args.pop(0)
+
+            if command['req_args_num'] == 0:
+                return (
+                    self.client.send_message(message.channel,
+                                             str(command['function']
+                                                 (message, self.client,
+                                                  args))
+                                             )
+                )
+            elif len(args) >= command['req_args_num']:
+                return (
+                    self.client.send_message(message.channel,
+                                             str(command['function']
+                                                 (message, self.client,
+                                                  args))
+                                             )
+                )
+            else:
+                return (
+                    self.client.send_message(message.channel,
+                                             'command "{}" requires {} '
+                                             'argument(s) "{}"'
+                                             .format(command['trigger'],
+                                                     command['args_num'],
+                                                     ', '.join(
+                                                         command['args_name']))
+                                             )
+                )
