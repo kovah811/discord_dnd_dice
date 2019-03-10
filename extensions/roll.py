19e1234efdb28cb0df1a4d87ed16979a11f2bb50
kovah811 <36887070+kovah811@users.noreply.github.com>
2019-03-10 15:04:29 +0000

    add get_valid_dice method to roll cog and create docstring stubs

--- extensions/roll.py
+++ extensions/roll.py
@@ -1,20 +1,53 @@
+import re
+
 import discord
 from discord.ext import commands
 
 
 class Roll:
+    """The Roll cog.
+
+    :param client: the discord client object
+    :type client: discord.ext.commands.Bot
+
+    """
+
+    DICE_PATTERN = re.compile(r"^(\d*)d(\d+)([-+]\d+)?$")
+    VALID_DICE = ['4', '6', '8', '10', '12', '20', '100']
+
     def __init__(self, client):
         self.client = client
 
     async def on_message_delete(self, message):
+        """The on_message_delete method"""
+
         await self.client.send_message(message.channel, 'Message deleted.')
 
-    @commands.command()
-    async def d20(self):
+    def get_valid_dice(self):
+        """Return string representation of valid dice types."""
+
+        return ', '.join(['d' + d for d in self.VALID_DICE])
+
+    @commands.command(pass_context=True)
+    async def d20(self, ctx):
+        """The d20 command.
+
+        :param ctx: the discord command context object
+        :type ctx: discord.ext.commands.Context
+
+        """
+
         await self.client.say('d20')
 
-    @commands.command()
-    async def roll(self):
+    @commands.command(pass_context=True)
+    async def roll(self, ctx):
+        """The roll command.
+
+        :param ctx: the discord command context object
+        :type ctx: discord.ext.commands.Context
+
+        """
+
         await self.client.say('roll')
 
 
