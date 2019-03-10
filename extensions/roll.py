7de23eca03f59530e05fc6454e69b98261ef2249
kovah811 <36887070+kovah811@users.noreply.github.com>
2019-03-10 15:06:36 +0000

    add d20 crit/fumble message method

--- extensions/roll.py
+++ extensions/roll.py
@@ -28,6 +28,30 @@ class Roll:
 
         return ', '.join(['d' + d for d in self.VALID_DICE])
 
+    @staticmethod
+    def get_d20_minmax_msg(crit, fumble):
+        """Return a message to append to results if d20 roll is 1 or 20.
+
+        :param crit: Whether or not a natural 20 was rolled
+        :type crit: bool
+        :param fumble: Whether or not a natural 1 was rolled
+        :type fumble: bool
+
+        """
+
+        msg = ''
+
+        if crit is True and fumble is True:
+            msg = ('  --  Natural 20 and natural 1!\n '
+                   'If rolling advantage, Crit!\n '
+                   'If rolling disadvantage, Fumble!')
+        elif crit is True:
+            msg = '  --  Natural 20! (Crit)'
+        elif fumble is True:
+            msg = '  --  Natural 1! (Fumble)'
+
+        return msg
+
     @commands.command(pass_context=True)
     async def d20(self, ctx):
         """The d20 command.
